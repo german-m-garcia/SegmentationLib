f665f775139696b5c606c4c0d2a8814677220ca3
German Martin <martin@ais.uni-bonn.de>
2016-07-25 12:18:07 +0200

    gestalts source file

---
+++ src/contours/Gestalts.cpp
@@ -0,0 +1,28 @@
+/*
+ * Gestalts.cpp
+ *
+ *  Created on: 20 Jul 2016
+ *      Author: martin
+ */
+
+#include "contours/Gestalts.h"
+
+Gestalts::Gestalts() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Gestalts::~Gestalts() {
+	// TODO Auto-generated destructor stub
+}
+
+
+double Gestalts::symmetry(Contour& c1, Contour&c2){
+	return 0.;
+}
+double Gestalts::similarity(Contour& c1, Contour&c2){
+	return 0.;
+}
+double Gestalts::parallelity(Contour& c1, Contour&c2){
+	return 0.;
+}
