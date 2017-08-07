ea9076887ccfe0fd780b1c48c9017c0c43767129
German Martin <martin@ais.uni-bonn.de>
2017-08-07 14:14:34 +0200

    cmake changes

--- vbs.sh
+++ vbs.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-folder=/home/martin/Datasets/VBS/Test
-output=/home/martin/Datasets/VBS/output
-seq=$folder/arctic_kayak
-scales=1
+scales=2
+#true=1
+#false=0
+gpu=0 
 
-for f in $seq/*.png; do
-    filename=$(basename "$f")
-	extension="${filename##*.}"
-	#filename="${filename%.*}"
-    
-    
-    ./segment $f $output/$filename $scales 0 0.05
+dir=(/home/martin/Datasets/VBS/Train/*/)
+for ((i = ${#dir[@]} - 1;i >= 0;i--)); do
+	    d=${dir[i]}	
+	    echo $d
+	    
+	    seq=$d
+	    output=$d/output
+	    mkdir -p $output
+		
+		for f in $seq/*.png; do
+			filename=$(basename "$f")
+			extension="${filename##*.}"
+			#filename="${filename%.*}"
+		
+		
+			#if the threshold is too small, too much noise from the edges shows up
+			#if it is too big, finer levels of the pyramid miss the details
+			build/segment $f $output/$filename $scales $gpu 0.05  
+		done
+		#  gdb --args 
+	    
+	    
+	    
 done
-#  gdb --args 
+
 
